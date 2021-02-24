30a826f4dc9e81e9d327f07f56f85949e463ad74
WagleTanvi <tanviw@hotmail.com>
2021-02-23 21:55:44 -0500

    dependencies script

---
+++ install-dependencies.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+wget http://content.mellanox.com/ofed/MLNX_OFED-5.0-2.1.8.0/MLNX_OFED_LINUX-5.0-2.1.8.0-rhel7.8-x86_64.tgz
+sudo yum install pciutils createrepo
+tar -xvzf MLNX_OFED_LINUX-5.0-2.1.8.0-rhel7.8-x86_64.tgz
+cd MLNX_OFED_LINUX-5.0-2.1.8.0-rhel7.8-x86_64
+sudo ./mlnxofedinstall --with-mft --with-mstflint --dpdk --upstream-libs --add-kernel-support
+sudo dracut -f
+sudo reboot
