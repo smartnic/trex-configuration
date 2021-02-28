76acfcc3fbc66cef5dea12710f23f0d552776a6b
WagleTanvi <tanviw@hotmail.com>
2021-02-28 16:42:03 -0500

    old trex version

--- install-trex.sh
+++ install-trex.sh
@@ -1,6 +1,6 @@
-wget --no-check-certificate --no-cache https://trex-tgn.cisco.com/trex/release/latest
-
+cd ~
+wget --no-check-certificate --no-cache https://trex-tgn.cisco.com/trex/release/v2.87.tar.gz
 tar -xzvf latest
-cd v2.87
-sudo cp  trex_cfg.yaml /etc/trex_cfg.yaml
+cd v2.*
+sudo cp  $HOME/trex-configuration/trex_cfg.yaml /etc/trex_cfg.yaml
 sudo ./dpdk_setup_ports.py -s
