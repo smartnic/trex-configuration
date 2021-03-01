6a0495674a4e329300e15c49de3c470dd3f329ca
WagleTanvi <tanviw@hotmail.com>
2021-03-01 11:18:08 -0500

    added udp_for_benchmarks

--- install-trex.sh
+++ install-trex.sh
@@ -1,6 +1,8 @@
 cd ~
 wget --no-check-certificate --no-cache https://trex-tgn.cisco.com/trex/release/v2.87.tar.gz
-tar -xzvf latest
+tar -xzvf v2.87.tar.gz
 cd v2.*
 sudo cp  $HOME/trex-configuration/trex_cfg.yaml /etc/trex_cfg.yaml
 sudo ./dpdk_setup_ports.py -s
+rm stl/udp_for_benchmarks.py
+sudo cp  $HOME/trex-configuration/udp_for_benchmarks.py stl/
