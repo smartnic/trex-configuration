26ca8645465a62ac8d765d8e6617697df7225db4
WagleTanvi <tanviw@hotmail.com>
2021-06-21 13:17:36 -0400

    small fix

--- katran/load_katran.sh
+++ katran/load_katran.sh
@@ -7,7 +7,7 @@ export interface
 mac_addr="9c:dc:71:49:98:31"
 export mac_addr
 file="./deps/bpfprog/bpf/balancer_kern.o"
-cd ../katran/_build
+cd ~/katran/_build
 echo "Starting Katran.  -v6 provides higher debug"
 nohup sudo ./build/example_grpc/katran_server_grpc -v 6 -balancer_prog $file -default_mac $mac_addr -hc_forwarding=false -intf=$interface  -lru_size=10000 > ~/log.txt &
 sleep 5
